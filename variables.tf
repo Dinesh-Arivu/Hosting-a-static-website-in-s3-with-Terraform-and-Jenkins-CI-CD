a79ee5a9e93607796fb5a64bb96dcde0f9e87e0e
DINESH THIYAGARAJAN <dineshthiyagu1097@gmail.com>
2025-08-19 13:06:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "staticwebsitehostings3withjenkinsandterraform"
+  default     = "staticwebsitehostings3withjenkinsandterraformfiles"
 }
 
 variable "acl" {
